bad44f4ade33788584106a384e4b7c79680091e6
Thomas Hankinson <96075370+thankinson@users.noreply.github.com>
2023-10-06 13:08:19 +0100

    Create rpiInstallDocker.sh

---
+++ rpiInstallDocker.sh
@@ -0,0 +1,31 @@
+#!bin/bash
+
+# This is my instalation script for raspbery pi4 home server
+
+# this removes docker
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+# instalaton setup
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg -y
+sudo install -m 0755 -d /etc/apt/keyrings 
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg -y
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+
+# this adds curent user to the list of docker users
+sudo usermod -a -G docker $USER
+
+# reboots pi to allow for changes to take effect
+sudo reboot 
